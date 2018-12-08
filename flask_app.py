3b98875ef5e309de3f90fe0482c410db370d67db
Moshe Jonathan Gordon Radian <vehpus@gmail.com>
2018-12-08 11:08:09 +0200

    Added support to get captions as a JSON repsonse, to support the chrome extention

--- flask_app.py
+++ flask_app.py
@@ -2,7 +2,7 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, request, make_response, render_template, redirect, url_for
+from flask import Flask, request, make_response, render_template, redirect, url_for, jsonify
 from fillindblanks import generate_fillindblanks, generate_fillindblanks_given_language
 import youtube_download as ydl
 from captions import map_initials_to_language_word
@@ -10,6 +10,18 @@ import traceback
 import youtube_dl
 
 app = Flask(__name__)
+
+
+def captions_from_yt_link(yt_link):
+    YDL = ydl.YDLWrapper(yt_link)
+    print("###############")
+    print(YDL.list_available_captions())
+    captions = map_initials_to_language_word(YDL.list_available_captions())
+    aut_captions = map_initials_to_language_word(
+        YDL.list_automatic_captions())
+    aut_captions = ["(Automatic) " + c for c in aut_captions]
+    captions.extend(aut_captions)
+    return captions
 
 
 def generate_fibd_response_given_language(yt_link, name, language, skip, output_type):
@@ -54,17 +66,11 @@ def homepage():
     return render_template('homepage.html')
 
 
-@app.route('/get_available_captions', methods=['GET','POST'])
-def get_available_captions():
+@app.route('/caption_selection', methods=['GET','POST'])
+def caption_selection():
     try:
         yt_link = request.form['yt_link']
-        YDL = ydl.YDLWrapper(yt_link)
-        print("###############")
-        print(YDL.list_available_captions())
-        captions = map_initials_to_language_word(YDL.list_available_captions())
-        aut_captions = map_initials_to_language_word(YDL.list_automatic_captions())
-        aut_captions = ["(Automatic) " + c for c in aut_captions]
-        captions.extend(aut_captions)
+        captions = captions_from_yt_link(yt_link)
         return render_template("caption_selection.html", yt_link=yt_link, captions=captions)
     except youtube_dl.utils.DownloadError as err:
         print(err)
@@ -72,6 +78,20 @@ def get_available_captions():
     except Exception as err:
         print(type(err))
         return "An error occured: {}\nTraceback:\n{}".format(str(err), traceback.format_exc())
+
+@app.route('/get_available_captions', methods=['GET','POST'])
+def get_available_captions():
+    try:
+        yt_link = request.form['yt_link']
+        captions = captions_from_yt_link(yt_link)
+        return jsonify(yt_link=yt_link, captions=captions)
+    except youtube_dl.utils.DownloadError as err:
+        print(err)
+        return "An error occured: {}\nTraceback:\n{}".format(str(err), traceback.format_exc())
+    except Exception as err:
+        print(type(err))
+        return "An error occured: {}\nTraceback:\n{}".format(str(err), traceback.format_exc())
+
 
 
 @app.route('/get_worksheet', methods=['GET','POST'])
